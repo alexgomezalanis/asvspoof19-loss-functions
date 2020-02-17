Fix angular softmax loss

angular_softmax_loss.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class AngularPenaltySMLoss(nn.Module):
 
-  def __init__(self, in_features, out_features, loss_type='sphereface', eps=1e-7, s=None, m=None):
+  def __init__(self, in_features, out_features, device, loss_type='sphereface', eps=1e-7, s=None, m=None):
     '''
     Angular Penalty Softmax Loss
     Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
@@ -30,7 +30,7 @@ class AngularPenaltySMLoss(nn.Module):
     self.loss_type = loss_type
     self.in_features = in_features
     self.out_features = out_features
-    self.fc = nn.Linear(in_features, out_features, bias=False)
+    self.fc = nn.Linear(in_features, out_features, bias=False).to(device)
     self.eps = eps
 
   def forward(self, x, labels):
@@ -41,8 +41,11 @@ class AngularPenaltySMLoss(nn.Module):
     assert torch.min(labels) >= 0
     assert torch.max(labels) < self.out_features
     
-    for W in self.fc.parameters():
-      W = F.normalize(W, p=2, dim=1)
+    for _, module in self.fc.named_modules():
+      if isinstance(module, nn.Linear):
+        module.weight.data = F.normalize(module.weight, p=2, dim=1)
+    #for W in self.fc.parameters():
+    #  W = F.normalize(W, p=2, dim=1)
 
     x = F.normalize(x, p=2, dim=1)
 

main.py
@@ -77,9 +77,9 @@ if __name__ == '__main__':
   if args.loss_method == 'softmax':
     criterion = nn.CrossEntropyLoss()
   elif args.loss_method == 'angular_softmax_sphereface':
-    criterion = AngularPenaltySMLoss(in_features=args.emb_size, out_features=args.num_classes, loss_type='sphereface')
+    criterion = AngularPenaltySMLoss(in_features=args.emb_size, out_features=args.num_classes, device=device, loss_type='sphereface')
   elif args.loss_method == 'angular_softmax_cosface':
-    criterion = AngularPenaltySMLoss(in_features=args.emb_size, out_features=args.num_classes, loss_type='cosface')
+    criterion = AngularPenaltySMLoss(in_features=args.emb_size, out_features=args.num_classes, device=device, loss_type='cosface')
 
   params = list(model.parameters()) + list(criterion.parameters())
   optimizer = optim.Adam(params, lr=args.lr)
@@ -158,6 +158,6 @@ if __name__ == '__main__':
         device=device,
         mp=mp)
     
-    
+
   
   