c3e3fe63f7b3ad0ef9508dbc344e596cef796e25
alexgomezalanis <alexgomezalanis@gmail.com>
2020-02-21 13:20:56 +0100

    Update softmax loss

--- kde_loss.py
+++ kde_loss.py
@@ -85,9 +85,20 @@ class KernelDensityLoss(nn.Module):
     # N spoofing classes, M utterances per class
     N, M, _ = list(self.log_probs.size())
 
+    softmax_loss = 0
+    for j in range(N):
+      for i in range(M):
+        softmax = -F.log_softmax(self.log_probs[j,i], 0)[j]
+        if softmax > 0:
+          softmax_loss += softmax
+
+    print('Softmax loss')
+    print(softmax_loss)
+    return softmax_loss
+
     ### WARNING ###
     # torch.sum() doesn't work fine. Issue: https://github.com/pytorch/pytorch/issues/5863
-    L = [-self.softmax(self.log_probs[j,i])[j] for i in range(M) for j in range(N)]
+    #L = [-self.softmax(self.log_probs[j,i])[j] for i in range(M) for j in range(N)]
     '''for j in range(N):
       L_row = []
       for i in range(M):
@@ -95,8 +106,8 @@ class KernelDensityLoss(nn.Module):
         #L_row.append(-F.log_softmax(self.log_probs[j,i], 0)[j])
       L_row = torch.stack(L_row)
       L.append(L_row)'''
-    L_torch = torch.stack(L)
-    return F.relu(L_torch).sum()
+    #L_torch = torch.stack(L)
+    #return F.relu(L_torch).sum()
 
   def contrast_loss(self, embeddings):
     # N spoofing classes, M utterances per class
